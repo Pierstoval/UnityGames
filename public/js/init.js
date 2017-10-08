190c5f0c7cc7d51bbb3c453a09516b2881fc5813
Alex Rock Ancelet <pierstoval@gmail.com>
2017-10-08 15:05:32 +0200

    Update OE links & notice about touch feature

--- public/js/init.js
+++ public/js/init.js
@@ -1,7 +1,14 @@
-(function($){
-  $(function(){
+(function ($, w) {
+    $(function () {
 
-    $('.button-collapse').sideNav();
+        $('.button-collapse').sideNav();
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+        $('.clickable').on('click', function () {
+            var link = $(this).find('a').first();
+            if (link) {
+                w.open($(link)[0].href, '_self');
+            }
+        });
+
+    }); // end of document ready
+})(jQuery, window); // end of jQuery name space
