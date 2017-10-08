30848e359b6e7575ba71aa38310b7c4e6608a6f5
Alex Rock Ancelet <pierstoval@gmail.com>
2017-10-08 23:13:52 +0200

    Added note trigger

--- public/js/init.js
+++ public/js/init.js
@@ -3,12 +3,5 @@
 
         $('.button-collapse').sideNav();
 
-        $('.clickable').on('click', function () {
-            var link = $(this).find('a').first();
-            if (link) {
-                w.open($(link)[0].href, '_self');
-            }
-        });
-
     }); // end of document ready
 })(jQuery, window); // end of jQuery name space
